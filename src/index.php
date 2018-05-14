f970af1c52426868eb6fa608fce227559abafc4f
Caroline <caroline.amaro@hotmail.com>
2018-05-14 17:02:54 -0300

    Exercicio #3 resolved

--- src/index.php
+++ src/index.php
@@ -10,6 +10,8 @@ $app = new App;
 $app->get('/', function (Request $request, Response $response, array $args) {
     $imagem = ImageManagerStatic::canvas(1200, 630, '#ff0000');
 
+    $imagem->insert("https://avatars2.githubusercontent.com/u/26804683?s=400&u=7279c9b6b8c513ef10892d8e9830a63399d29343&v=4");
+
     $response->write($imagem->encode()->getEncoded());
     return $response->withHeader('Content-Type', FILEINFO_MIME_TYPE);
 });
