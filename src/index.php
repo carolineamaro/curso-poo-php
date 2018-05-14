0981137ed0f98e819428803020c956af3b70bf5e
Caroline <caroline.amaro@hotmail.com>
2018-05-14 16:57:56 -0300

    Exercicio #1 resolved

--- src/index.php
+++ src/index.php
@@ -1,5 +1,16 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
 
-$ola =  "Seja bem vindo ao curso de POO com PHP";
+use Slim\App;
+use Slim\Http\Request;
+use Intervention\Image\ImageManagerStatic;
+use Slim\Http\Response;
 
+$app = new App;
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $imagem = ImageManagerStatic::canvas(1200, 630, '#ff0000');
+
+    $response->write($imagem->encode()->getEncoded());
+    return $response->withHeader('Content-Type', FILEINFO_MIME_TYPE);
+});
+$app->run();
